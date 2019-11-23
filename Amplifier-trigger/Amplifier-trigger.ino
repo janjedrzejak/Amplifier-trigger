62d69f6489ea1342b93f5d0d4a9d0362e087300d
Jan Jędrzejak <43744269+janjedrzejak@users.noreply.github.com>
2019-11-23 18:58:11 +0100

    Update Amplifier-trigger.ino
    Trigg 6 outputs after push button

--- Amplifier-trigger/Amplifier-trigger.ino
+++ Amplifier-trigger/Amplifier-trigger.ino
@@ -1,25 +1,31 @@
+int i = 0;
+int buttonState = 0;
+const int buttonPin = 8;
+
 void setup() {
-  Serial.begin(57600);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
+  Serial.begin(9600);
+  for(i = 2; i < 8; i++) {
+    pinMode(i, OUTPUT);
+  }
+    pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  digitalWrite(2, HIGH);
-  delay(2000);
-  digitalWrite(3, HIGH);
-  delay(2000);
-  digitalWrite(4, HIGH);
-  delay(2000);
-  digitalWrite(5, HIGH);
-  delay(2000);
-  digitalWrite(6, HIGH);
-  delay(2000);
-  digitalWrite(7, HIGH);
-  delay(2000);
-
+  buttonState = digitalRead(buttonPin);
+  if(buttonState == HIGH) {
+    digitalWrite(2, HIGH);
+    delay(2000);
+    digitalWrite(3, HIGH);
+    delay(2000);
+    digitalWrite(4, HIGH);
+    delay(2000);
+    digitalWrite(5, HIGH);
+    delay(2000);
+    digitalWrite(6, HIGH);
+    delay(2000);
+    digitalWrite(7, HIGH);
+    delay(2000);
+  }
+  
+  
 } 
